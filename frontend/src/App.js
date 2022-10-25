37a5ddf2c231181932e9ecedd173e0646a5d3a07
Kim Seok Yoon <kjkksu2@naver.com>
2022-10-25 21:56:48 +0900

    frontend settings

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  color: peru;
+`;
+
+const App = () => {
+  return <Container>bye</Container>;
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  name: "VWX",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.join(__dirname, "public") },
+    port: 3000,
+    hot: true,
+  },
+};
